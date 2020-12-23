ba88d53336bf084772054d7c3182fbbf15714b54
nike1v <dgygurdenok@gmail.com>
2020-12-23 13:40:21 +0200

    commit

--- App.js
+++ App.js
@@ -24,7 +24,6 @@ const App = () => (
 		<Container>
 			<Content>
 				<View style={styles.container}>
-					{console.log('text')}
 					<Text>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
 						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
